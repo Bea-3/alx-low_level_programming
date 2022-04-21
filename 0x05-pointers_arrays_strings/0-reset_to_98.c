e1c163a9140f6e67f1cc3ece82ef7bf7aa824ab1
Bea-3 <beatrice.pepple@gmail.com>
2022-04-21 13:37:35 -0700

    Task 0.98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a function that takes a pointer to an int 
+ * and updates the value to 98
+ * Return: Always 0
+ */
+int main(void)
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
