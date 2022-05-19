fa584c4df8c94da71d0d72c4dc26b98c7a03c224
Bea-3 <101516374+Bea-3@users.noreply.github.com>
2022-05-20 00:20:10 +0100

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,33 +1,54 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _realloc -  reallocates a memory block using malloc and free
- * @ptr: pointer
- * @old_size: old size
- * @new_size: new size
- * Return: pointer
+ *_realloc - reallocates a memory block using malloc and free
+ *
+ *@ptr: original pointer to be reallocated
+ *@old_size: size of old memory allocation in bytes
+ *@new_size: size of new memory allocation in bytes
+ *
+ *Return: a pointer to the new memory allocation or NULL if the process fails
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, *relloc;
-	unsigned int i;
+	int *nptr, *p;
+	unsigned int c;
 
-	if (ptr != NULL)
-	clone = ptr;
-	else
-	{ return (malloc(new_size)); }
-	if (new_size == old_size)
-	return (ptr);
-	if (new_size == 0 && ptr != NULL)
-	{ free(ptr);
-	return (0); }
-	relloc = malloc(new_size);
-	if (relloc == NULL)
-	return (0);
-	for (i = 0; i < (old_size || i < new_size); i++)
+	if (old_size == new_size)
 	{
-		*(relloc + i) = clone[i];
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	p = ptr;
+	nptr = malloc(new_size);
+	if (nptr == NULL)
+	{
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		return (nptr);
+	}
+	if (new_size > old_size)
+	{
+		for (c = 0; c < old_size; c++)
+		{
+			nptr[c] = p[c];
+		}
+	}
+	else
+	{
+		for (c = 0; c < new_size; c++)
+		{
+			nptr[c] = p[c];
+		}
 	}
 	free(ptr);
-return (relloc);
+	return (nptr);
 }
