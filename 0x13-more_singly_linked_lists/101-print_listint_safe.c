a7d9625de90b7000572667a43c5f969b414e19d9
Bea-3 <101516374+Bea-3@users.noreply.github.com>
2022-05-26 23:23:20 +0100

    commit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,49 +1,30 @@
 #include "lists.h"
+
 /**
- * isCycleList - checks if a list is a cycle
- * @head: listint_t list
- * Return: a pointer
- */
-const listint_t *isCycleList(const listint_t *head)
-{
-const listint_t *fast = head;
-const listint_t *slow = head;
-while (fast && fast->next)
-{
-fast = fast->next->next;
-slow = slow->next;
-if (fast == slow)
-{
-slow = head;
-while (slow != fast)
-{
-fast = fast->next;
-slow = slow->next;
-}
-return (slow);
-}
-}
-return (NULL);
-}
-/**
- * print_listint_safe - function
- * @head: listint_t list
- * Return: the number of nodes in the list
+ * print_listint_safe - prints list, avoids loop
+ * @head: points to start of list
+ * Return: number of nodes in size_t or exit 98
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *breakPoint = isCycleList(head);
-int ommit = 0;
-int count = 0;
-while (head && ommit != 2)
-{
-printf("[%p] %d\n", (void *) head, head->n);
-head = head->next;
-if (head == breakPoint)
-ommit++;
-count++;
-}
-if (ommit == 2)
-printf("-> [%p] %d\n", (void *) breakPoint, breakPoint->n);
-return (count);
+	/* declarations */
+	size_t nodeCount = 0;
+	/* check for null pointer */
+	while (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		nodeCount += 1;
+
+		if (head > head->next)
+		{
+			head = head->next;
+		}
+		else
+		{
+			head = head->next;
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+	}
+	return (nodeCount);
 }
