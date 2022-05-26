6667c216c740955389af19cc1188e5f0fae32eb4
Bea-3 <101516374+Bea-3@users.noreply.github.com>
2022-05-26 23:14:19 +0100

    commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,37 +1,40 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - delete a node at index
- * @head: ptr to ptr to list
- * @index: where to delete node
- * Return: 1 on success or -1 on fail
- */
+* delete_nodeint_at_index - deletes node.
+* @head: head pointer.
+* @index: index delete
+* Return: 1 ok
+* fail -1
+*/
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *prev;
+	listint_t *nxt;
 	unsigned int i;
-	listint_t *location;
-	listint_t *temp = NULL;
 
-	if (!(head && *head))
+	if (head == NULL || *head == NULL)
 		return (-1);
-	location = *head;
+
+	nxt = *head;
+	prev = NULL;
 	if (index == 0)
 	{
-		temp = location;
-		*head = location->next;
-		free(temp);
+		*head = (*head)->next;
+		free(nxt);
 		return (1);
 	}
-	else
-		for (i = 0; i < (index - 1); i++)
-		{
-			if (location->next)
-			location = location->next;
-			else
-				return (-1);
-		}
-		temp = location->next;
-		location->next = temp->next;
-	free(temp);
+	for (i = 0; i < index; i += 1)
+	{
+		if (nxt == NULL)
+			return (-1);
+		prev = nxt;
+		nxt = nxt->next;
+	}
+	if (prev)
+		prev->next = nxt->next;
+	free(nxt);
+
 	return (1);
 }
